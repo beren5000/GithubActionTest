fbfca71fb33a8dda671f03b43889e4c43cb65c88
camilo.monsalve <camilo.monsalve@xpo.com>
2021-11-17 14:59:16 -0500

    test

--- apps/action_test/tests/tests.py
+++ apps/action_test/tests/tests.py
@@ -4,3 +4,8 @@ from django.test import TestCase
 class TestsTest(TestCase):
     def test_simple_assertion(self):
         self.assertEqual("1", str(1))
+
+
+class TestsBadTest(TestCase):
+    def test_simple_bad_assertion(self):
+        self.assertEqual("1", 1)
